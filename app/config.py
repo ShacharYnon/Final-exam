cc6870e2d40b096a36d044350e667d0f2d37c316
Shachar <shar535909@gmail.com>
2025-09-08 09:44:58 +0300

    Building a Kafka pub without tests

--- app/config.py
+++ app/config.py
@@ -2,3 +2,9 @@ import os
 
 
 FILE_PATH = os.getenv("FILE_PATH" , "C:/Users/Work/OneDrive/Desktop/The-muezzin/podcasts/download (1).wav")
+
+KAFKA_BOOTSTRAP = os.getenv("SERVER_ADDRESS", "localhost:9092")
+
+TOPIC_ = os.getenv("TOPIC", "topic_publisher")
+MESSAGE_ = os.getenv("MESSAGE" ,"hello word") 
+
--- app/kafka/pub.py
+++ app/kafka/pub.py
@@ -3,6 +3,7 @@ import logging
 logger = logging.getLogger(__name__) 
 from bson import json_util
 from ..loading.expenditure import Extracting_Metadata
+from .. import config
 
 class Publisher:
     def __init__(self ,kafka_bootstrap):
@@ -11,4 +12,20 @@ class Publisher:
             bootstrap_servers=kafka_bootstrap,
             value_serializer=lambda v: json_util.dumps(v).encode('utf-8'),
         )
+        self.logger = logging.getLogger("Publisher")
+
+    def publish(self ,topic, message):
+        self.producer.send(topic, value=message)
+        self.logger.info(f"Sent to {topic}: {message}")
+
+
+    def close(self):
+        self.producer.close()
+
+if __name__ == "main":
+    pub = Publisher(config.KAFKA_BOOTSTRAP)
+    pub.publish(config.TOPIC_ ,config.MESSAGE_)
+
+
+
 
