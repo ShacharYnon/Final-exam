f43686fc9bba54105f03991847365e734dd481a7
Shachar <shar535909@gmail.com>
2025-09-08 16:44:36 +0300

    Getting the information in Jason to Kafka and the pub sends

--- app/kafka/pub.py
+++ app/kafka/pub.py
@@ -21,23 +21,32 @@ class Publisher:
         )
         logger.info(f"KafkaProducer created. bootstrap={kafka_bootstrap}")
 
-    def publish(self ,topic, message):
+    def publish(self ,topic, messages):
+        count = 0
         try:
-            self.producer.send(topic, value=message)
-            logger.info(f"Sent to {topic}: {message}")
+            for message in messages:
+                self.producer.send(topic, value=message)
+                count += 1
+            self.producer.flush()
+            logger.info(f"Published {count} message(s) to topic '{topic}'.")
         except Exception as e:
-            logger.error(f"ERROR: From publish : {e} ")
+            logger.error(f"Failed to publish to '{topic}': {e}")
+            raise RuntimeError(f"Failed to publish to '{topic}': {e}")
+        #     self.producer.send(topic, value=message)
+        #     logger.info(f"Sent to {topic}: {message}")
+        # except Exception as e:
+        #     logger.error(f"ERROR: From publish : {e} ")
 
 
     def close(self):
-        
         time.sleep(30)
         self.producer.close()
 
-if __name__ == "__main__":
-    pub = Publisher(config.KAFKA_BOOTSTRAP)
-    pub.publish(config.TOPIC_ ,config.MESSAGE_)
-    pub.close()
+# if __name__ == "__main__":
+    
+#     pub = Publisher(config.KAFKA_BOOTSTRAP)
+#     pub.publish(config.TOPIC_ ,config.MESSAGE_)
+#     pub.close()
 
 
 # python -m app.kafka.pub
